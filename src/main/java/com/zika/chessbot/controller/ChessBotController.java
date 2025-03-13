733c9481f89da1b03dc0e736c23d585f9729477f
Jonnas <jonnas.oliveira@sptech.school>
2025-03-13 11:30:20 -0300

    Apagado umas coisas na controller de debug que esqueci kk

--- src/main/java/com/zika/chessbot/controller/ChessBotController.java
+++ src/main/java/com/zika/chessbot/controller/ChessBotController.java
@@ -1,14 +1,8 @@
 package com.zika.chessbot.controller;
 
-import com.github.bhlangonijr.chesslib.Board;
-import com.github.bhlangonijr.chesslib.Square;
-import com.github.bhlangonijr.chesslib.game.GameContext;
-import com.zika.chessbot.bot.BoardUtils;
 import com.zika.chessbot.bot.ChessBot;
 import com.zika.chessbot.bot.utils.BishopUtils;
-import com.zika.chessbot.bot.utils.BitBoardUtils;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -19,16 +13,9 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class ChessBotController {
     private final ChessBot chessBot;
-    private final BishopUtils bishopUtils;
 
     @GetMapping(value = "/movimento")
     public Map<String,String> calcularMovimento(@RequestParam String fenString) {
         return Map.of("move", chessBot.decideMove(fenString));
-    }
-
-    @GetMapping(value = "/teste")
-    public ResponseEntity<Void> teste() {
-        bishopUtils.countBlockedDiagonals(new Board(new GameContext(), true), Square.E4);
-        return ResponseEntity.noContent().build();
     }
 }
